87b1b34c5992ba99a11a6174b5370b1aa2cbf9b2
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-08-04 11:48:37 -0400

    animation keyframes

---
+++ littlest_tokyo/script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/webgl_animation_keyframes.html
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/main.css
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/jsm/controls/OrbitControls.js
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/jsm/environments/RoomEnvironment.js
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/jsm/loaders/GLTFLoader.js
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/jsm/loaders/DRACOLoader.js
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/jsm/libs/stats.module.js
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/libs/draco/gltf/draco_decoder.js
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/libs/draco/gltf/draco_decoder.wasm
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/libs/draco/gltf/draco_decoder.js
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/libs/draco/gltf/draco_wasm_wrapper.js
+wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/libs/draco/gltf/draco_encoder.js
