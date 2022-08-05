25a55e079ce2468d016ac5dd75e02a0e9fb90537
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-08-05 15:31:59 -0400

    meilleure

--- Code/littlest_tokyo/script.sh
+++ Code/littlest_tokyo/script.sh
@@ -4,9 +4,11 @@ DIR="js/libs/draco/gltf"
 mkdir -p "$DIR"
 cd "$DIR"
 
-wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/libs/draco/gltf/draco_decoder.js
-wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/libs/draco/gltf/draco_decoder.wasm
-wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/libs/draco/gltf/draco_decoder.js
-wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/libs/draco/gltf/draco_wasm_wrapper.js
-wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/libs/draco/gltf/draco_encoder.js
+GLTF="https://raw.githubusercontent.com/mrdoob/three.js/master/examples/js/libs/draco/gltf"
+wget "$GLTF/draco_decoder.js"
+wget "$GLTF/draco_decoder.wasm"
+wget "$GLTF/draco_decoder.js"
+wget "$GLTF/draco_wasm_wrapper.js"
+wget "$GLTF/draco_encoder.js"
 
+echo "fini"
