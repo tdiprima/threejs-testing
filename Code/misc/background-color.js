dffcfaf2dc1e4de035fc59c3385a2c8c3e437847
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-09-30 14:01:41 -0400

    hmm

--- Code/misc/background-color.js
+++ Code/misc/background-color.js
@@ -4,6 +4,6 @@ scene.background = new THREE.Color(0xcce0ff);
 
 renderer.setClearColor(0xcce0ff); // sky blue background - replace 0xcce0ff with any hex color
 
-// If you want a transparent background you will have to enable alpha in your renderer first:
+// If you want a transparent background, you will have to enable alpha in your renderer first:
 renderer = new THREE.WebGLRenderer({alpha: true}); // init like this
 renderer.setClearColor(0xffffff, 0); // second param is opacity, 0 => transparent
