c244c6facf5d41f7f6774682c85529e38f7d61d7
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-10-18 15:22:33 -0400

    mm

--- Code/misc/dumpObject.js
+++ Code/misc/dumpObject.js
@@ -1,5 +1,6 @@
 /**
  * Dump the scenegraph to the JavaScript console.
+ * http://127.0.0.1:5500/manual/#en/load-gltf
  */
 function dumpObject(obj, lines = [], isLast = true, prefix = '') {
   const localPrefix = isLast ? '└─' : '├─';
