89f6f237913f687c023a9292db4c4025ade23a6e
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-10-19 14:11:15 -0400

    mmm

--- Code/rotating_cube/textures/script.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/crate.gif
-wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/brick_bump.jpg
-wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/brick_diffuse.jpg
-wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/disturb.jpg
-wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/metal.jpg
-wget https://raw.githubusercontent.com/mrdoob/three.js/master/examples/textures/golfball.jpg
