874d5d9ebcc5932ec39bc8fc898ad76252c85faa
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-10-24 11:16:35 -0400

    Update tick.js

--- Code/misc/tick.js
+++ Code/misc/tick.js
@@ -1,6 +1,9 @@
 /**
  * https://threejs-journey.com/lessons/animations#using-requestanimationframe
  * https://threejs-journey.com/lessons/animations#adaptation-to-the-framerate
+ * To adapt the animation to the framerate, we need to know how much time it's been since the last tick.
+ * When we base our rotation on how much time was spent since the last frame, this rotation speed will be the same
+ * on every screen and every computer regardless of the frame rate.
  */
 let time = Date.now();
 
@@ -13,6 +16,7 @@ const tick = () => {
   // Update objects
   // mesh.rotation.y += 0.01;
   mesh.rotation.y += 0.01 * deltaTime;
+  // console.log("deltaTime", deltaTime);
 
   // Render
   renderer.render(scene, camera);
