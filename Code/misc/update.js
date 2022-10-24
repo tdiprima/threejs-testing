50173c0519c48341a3ec7dffbae46018fb07eb34
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-10-24 16:35:52 -0400

    mmm

---
+++ Code/misc/update.js
@@ -0,0 +1,16 @@
+/**
+ * The idea behind the update pattern is to make each object in the scene responsible for updating itself.
+ * https://discourse.threejs.org/t/whats-the-best-simple-way-to-handle-the-update-pattern/13719
+ */
+function update() {
+  const delta = clock.getDelta();
+
+  scene.traverse(child => {
+    if (typeof child.update === 'function') child.update(delta);
+  });
+}
+
+renderer.setAnimationLoop(() => {
+  update();
+  render();
+});
