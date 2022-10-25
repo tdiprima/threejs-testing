345038bf2b30fcf63e5249894082edf08a3a2635
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-10-25 10:20:17 -0400

    Create textures.js

---
+++ Code/misc/textures.js
@@ -0,0 +1,20 @@
+// EASY.
+let loader = new THREE.TextureLoader();
+  let material = new THREE.MeshBasicMaterial({
+    map: loader.load(img),
+    side: THREE.DoubleSide
+  });
+
+  let geometry = new THREE.PlaneGeometry(10, 10);
+  let mesh = new THREE.Mesh(geometry, material);
+  mesh.position.set(0, 0, 0);
+  scene.add(mesh);
+
+// GET DIMENSIONS.
+// https://stackoverflow.com/questions/49111431/how-to-get-texture-dimensions-with-three-js
+  var loader = new THREE.TextureLoader();
+  var texture = loader.load( "./img.png", function ( tex ) {
+    // tex and texture are the same in this example, but that might not always be the case
+    console.log( tex.image.width, tex.image.height );
+    console.log( texture.image.width, texture.image.height );
+  });
