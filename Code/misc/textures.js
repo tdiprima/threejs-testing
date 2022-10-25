e881ffecc6015ba64b0c9f4148d2aaff287b3b65
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-10-25 10:45:32 -0400

    mmm

--- Code/misc/textures.js
+++ Code/misc/textures.js
@@ -1,20 +1,24 @@
+let img = "/textures/images/bear-soongnyoong.png";
+let loader;
+
 // EASY.
-let loader = new THREE.TextureLoader();
-  let material = new THREE.MeshBasicMaterial({
-    map: loader.load(img),
-    side: THREE.DoubleSide
-  });
+loader = new THREE.TextureLoader();
+let material = new THREE.MeshBasicMaterial({
+  map: loader.load(img),
+  side: THREE.DoubleSide
+});
 
-  let geometry = new THREE.PlaneGeometry(10, 10);
-  let mesh = new THREE.Mesh(geometry, material);
-  mesh.position.set(0, 0, 0);
-  scene.add(mesh);
+let geometry = new THREE.PlaneGeometry(10, 10);
+let mesh = new THREE.Mesh(geometry, material);
+mesh.position.set(0, 0, 0);
+scene.add(mesh);
 
-// GET DIMENSIONS.
+// ADVANCED.
+// https://stackoverflow.com/questions/51722754/three-js-image-aspect-ratio
 // https://stackoverflow.com/questions/49111431/how-to-get-texture-dimensions-with-three-js
-  let loader = new THREE.TextureLoader();
-  let texture = loader.load(img, function ( tex ) {
-    // tex and texture are the same in this example, but that might not always be the case
-    console.log( tex.image.width, tex.image.height );
-    console.log( texture.image.width, texture.image.height );
-  });
+loader = new THREE.TextureLoader();
+let texture = loader.load(img, tex => {
+  // tex and texture are the same in this example, but that might not always be the case
+  console.log(tex.image.width, tex.image.height);
+  console.log(texture.image.width, texture.image.height);
+});
