648115862265c69e8c4ae5fc4551461a1bb33a29
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-11-16 16:31:21 -0500

    Update circumference.js

--- Code/misc/circumference.js
+++ Code/misc/circumference.js
@@ -1,10 +1,16 @@
-// Calculate the area and perimeter of a circle
+// https://www.w3resource.com/javascript-exercises/javascript-object-exercise-9.php
+// http://people.wku.edu/tom.richmond/Pir2b.html
+
+// Calculate the area and perimeter (circumference) of a circle
 function Circle(radius) {
   this.radius = radius;
+
   // area method
   this.area = function() {
+    // pi times the square of the radius
     return Math.PI * this.radius * this.radius;
   };
+
   // perimeter method
   this.perimeter = function() {
     return 2 * Math.PI * this.radius;
@@ -12,5 +18,5 @@ function Circle(radius) {
 }
 
 const c = new Circle(3);
-console.log('Area =', c.area().toFixed(2));
-console.log('Perimeter =', c.perimeter().toFixed(2));
+console.log("Area =", c.area().toFixed(2));
+console.log("Perimeter =", c.perimeter().toFixed(2));
