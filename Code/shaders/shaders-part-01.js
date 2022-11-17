1af0e2e3ffaaef7db642a7d2b02d70e25581e411
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2022-11-17 09:58:31 -0500

    finito

--- Code/shaders/shaders-part-01.js
+++ Code/shaders/shaders-part-01.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import SceneInit from './SceneInit.js';
+import SceneInit from "./SceneInit.js";
 
 const test = new SceneInit("myThreeJsCanvas");
 
@@ -16,10 +16,50 @@ test.scene.add(axesHelper);
 // test.scene.add(boxMesh);
 
 // part 1 - boilerplate code
+// const boxGeometry = new THREE.BoxGeometry(16, 16, 16, 16, 16, 16);
+// const boxMaterial = new THREE.MeshStandardMaterial({
+//   color: 0xff0000,
+//   wireframe: true
+// });
+//
+// const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+// test.scene.add(boxMesh);
+
+// part 3 - basics of glsl shaders
 const boxGeometry = new THREE.BoxGeometry(16, 16, 16, 16, 16, 16);
-const boxMaterial = new THREE.MeshStandardMaterial({
-  color: 0xff0000,
-  wireframe: true
+const boxMaterial = new THREE.ShaderMaterial({
+  wireframe: true,
+  vertexShader: `
+  void main()	{
+    // projectionMatrix, modelViewMatrix, position -> passed in from three.js
+
+    // gl_Position = projectionMatrix
+    //   * modelViewMatrix
+    //   * vec4(position.x, position.y, position.z, 1.0);
+
+    // gl_Position = projectionMatrix
+    //   * modelViewMatrix
+    //   * vec4(position.x, sin(position.z), position.z, 1.0);
+
+    // gl_Position = projectionMatrix
+    //   * modelViewMatrix
+    //   * vec4(position.x, sin(position.z) + position.y, position.z, 1.0);
+
+    // gl_Position = projectionMatrix
+    //   * modelViewMatrix
+    //   * vec4(position.x, sin(position.z/4.0) + position.y, position.z, 1.0);
+
+    gl_Position = projectionMatrix
+      * modelViewMatrix
+      * vec4(position.x, 4.0*sin(position.z/4.0) + position.y, position.z, 1.0);
+  }
+  `,
+  fragmentShader: `
+  void main() {
+    // gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+  }
+  `
 });
 
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
