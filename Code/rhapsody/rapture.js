5720c5457053d39e8b7f971e71a726d2b0aaad0d
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-01-16 14:37:08 -0500

    mmm

--- Code/rhapsody/rapture.js
+++ Code/rhapsody/rapture.js
@@ -2,60 +2,86 @@ const baseUrl = "http://localhost:8888/iiif/?iiif=http://localhost:8888/HalcyonS
 const segmentation = "http://localhost:8888/halcyon/?iiif=file:///D:/HalcyonStorage/nuclearsegmentation2019/TCGA-CM-5348-01Z-00-DX1.2ad0b8f6-684a-41a7-b568-26e97675cce9.zip";
 const feature = "http://localhost:8888/halcyon/?iiif=file:///D:/HalcyonStorage/features/raj/Ptumor_heatmap_TCGA-CM-5348-01Z-00-DX1.2ad0b8f6-684a-41a7-b568-26e97675cce9.zip";
 
+const magicNumber = 82984;
+
+/**
+ * WINDOW RESIZE HANDLER
+ */
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.updateProjectionMatrix();
 }
 
+/** @function */
 function srcUrl(x, y, w, h) {
   return new THREE.TextureLoader().load(
     `${baseUrl}/${x},${y},${w},${h}/512,/0/default.jpg`
   );
 }
 
+/** @function */
 function srcSegUrl(x, y, w, h) {
   return new THREE.TextureLoader().load(
     `${segmentation}/${x},${y},${w},${h}/512,/0/default.png`
   );
 }
 
-// FEATURE NOT USED
+/**
+ * GET FEATURES
+ * Note! We were doing load() before; now we're doing fetch().
+ */
 async function featureSrcUrl(x, y, w, h) {
   const response = await fetch(
     `${feature}/full/323,/0/default.json`
   );
-  const json = await response.json();
-  return json;
+  return await response.json();
 }
 
-function Square(x, y, w, h, src, offset) {
+/**
+ * WHY?
+ */
+function square(x, y, w, h, src, offset) {
   const texture = src;
+
   const square = new THREE.Shape();
   square.moveTo(0, 0);
   square.lineTo(0, 1);
   square.lineTo(1, 1);
   square.lineTo(1, 0);
+  // unfinished?
+  // and btw, we went backwards.
+
   const geometry = new THREE.ShapeGeometry(square);
   geometry.center();
+
   const material = new THREE.MeshBasicMaterial({ map: texture, depthWrite: false, side: THREE.DoubleSide });
+
   const X = new THREE.Mesh(geometry, material);
   X.scale.x = w;
   X.scale.y = h;
   X.frustumCulled = false;
   X.position.set(0, 0, offset);
+
   return X;
 }
 
-function SegmentationSquare(x, y, w, h, src, offset) {
+/**
+ * SAME EXACT FREAKIN THING AS SQUARE()
+ */
+function segmentationSquare(x, y, w, h, src, offset) {
   const texture = src;
+
   const square = new THREE.Shape();
   square.moveTo(0, 0);
   square.lineTo(0, 1);
   square.lineTo(1, 1);
   square.lineTo(1, 0);
+
   const geometry = new THREE.ShapeGeometry(square);
   geometry.center();
+
+  // Added: transparent: true, opacity: 0.5
   const material = new THREE.MeshBasicMaterial({
     map: texture,
     depthWrite: false,
@@ -63,15 +89,20 @@ function SegmentationSquare(x, y, w, h, src, offset) {
     transparent: true,
     opacity: 0.5
   });
+
   const X = new THREE.Mesh(geometry, material);
   X.scale.x = w;
   X.scale.y = h;
   X.frustumCulled = false;
   X.position.set(0, 0, offset);
+
   return X;
 }
 
-// GET FROM SEGMENTATION URL
+/**
+ * GET SEGMENTATION DATA
+ * UNUSED FUNCTION
+ */
 function getJSON(x, y, w, h) {
   const loader = new THREE.FileLoader();
   const json = loader.load(
@@ -80,7 +111,7 @@ function getJSON(x, y, w, h) {
 
     // onLoad callback
     data => {
-      //  console.log(data);
+      // console.log(data);
       const wow = JSON.parse(data);
       console.log(wow);
       return wow;
@@ -96,81 +127,111 @@ function getJSON(x, y, w, h) {
       console.error('An error happened');
     }
   );
-  console.log('OUT!');
+  console.log("OUT!");
 
   return json;
 }
 
-function BasicSquare(tile) {
+/**
+ * BASIC POLYGON
+ */
+function basicPolygon(tile) {
   // console.log(tile);
-  const poly = new THREE.Shape();
   const coords = tile.coordinates;
-  const hasValue = tile.hasValue;
+  const value = tile.hasValue; // not a boolean
+
+  const poly = new THREE.Shape();
   poly.moveTo(0, 0);
   poly.lineTo(0, 1);
   poly.lineTo(1, 1);
   poly.lineTo(1, 0);
+
   // for (let i = 1; i < coords.length; i++) {
-  //  poly.moveTo(coords[i].x/248, coords[i].y/248);
+  //  poly.moveTo(coords[i].x / 248, coords[i].y / 248);
   // }
+
   const geometry = new THREE.ShapeGeometry(poly);
   geometry.center();
-  const green = 255 * (1 - hasValue);
-  const blue = 255 * (1 - hasValue);
-  const red = 255 * hasValue;
+
+  const green = 255 * (1 - value);
+  const red = 255 * value;
+
   const material = new THREE.MeshBasicMaterial({
     color: new THREE.Color(red, green, 0).getHex(),
     transparent: true,
     opacity: 0.5
   });
+
   const X = new THREE.Mesh(geometry, material);
   X.scale.x = 347;
   X.scale.y = 347;
   X.frustumCulled = true;
-  //
+
   const d = 347 * 239;
-  X.position.set(347 * coords[0][0] - 82984 / 2, d - 347 * coords[0][1] - 82984 / 2, 1);
+  X.position.set(
+    347 * coords[0][0] - magicNumber / 2,
+    d - 347 * coords[0][1] - magicNumber / 2,
+    1
+  );
+
   return X;
 }
 
+/**
+ * @class Rapture
+ */
 class Rapture extends THREE.Object3D {
   constructor(x, y, w, h, offset) {
     super();
+
     this.isRapture = true;
     this.type = 'Rapture';
     this.booted = false;
+
     const lod = new THREE.LOD();
 
     // FROM THE BASE URL
-    const low = Square(x, y, w, h, srcUrl(x, y, w, h), offset);
+    const low = square(x, y, w, h, srcUrl(x, y, w, h), offset);
     lod.addLevel(low, w);
+
     low.onBeforeRender = function() {
       if (w > 1024) {
         if (!this.booted) {
           this.booted = true;
+
           const offx = Math.trunc(w / 2);
           const offy = Math.trunc(h / 2);
+
           const nw = new Rapture(x, y, offx, offy);
           const ne = new Rapture(x + offx, y, offx, offy);
+
           const sw = new Rapture(x, y + offy, offx, offy);
           const se = new Rapture(x + offx, y + offy, offx, offy);
+
           sw.position.set(-offx / 2, -offy / 2, 0);
           nw.position.set(-offx / 2, offy / 2, 0);
           se.position.set(offx / 2, -offy / 2, 0);
           ne.position.set(offx / 2, offy / 2, 0);
+
           const high = new THREE.Group();
           high.add(nw);
           high.add(ne);
           high.add(sw);
           high.add(se);
+
           lod.addLevel(high, w / 2);
         }
       }
     };
+
     return lod;
+
   }
 }
 
+/**
+ * @class FeatureLayer2
+ */
 class FeatureLayer2 extends THREE.Object3D {
   constructor(x, y, w, h, offset) {
     super();
@@ -184,7 +245,7 @@ class FeatureLayer2 extends THREE.Object3D {
     const lod = new THREE.LOD();
 
     // FROM THE SEGMENTATION URL
-    const low = SegmentationSquare(x, y, w, h, srcSegUrl(x, y, w, h), offset);
+    const low = segmentationSquare(x, y, w, h, srcSegUrl(x, y, w, h), offset);
     lod.addLevel(low, w);
     low.onBeforeRender = function() {
       if (w > 512) {
@@ -269,6 +330,10 @@ class FeatureLayer2 extends THREE.Object3D {
   }
 }
 
+/**
+ * @class FeatureLayer
+ * UNUSED CLASS
+ */
 class FeatureLayer extends THREE.Object3D {
   constructor(datum) {
     super();
@@ -277,47 +342,68 @@ class FeatureLayer extends THREE.Object3D {
     const matrix = new THREE.Group();
     console.log(`# of tiles = ${datum.length}`);
     for (let i = 0; i < datum.length; i++) {
-      // if (datum[i].hasValue>0.50) {
-      const tile = BasicSquare(datum[i]);
+      // if (datum[i].hasValue > 0.50) {
+      const tile = basicPolygon(datum[i]);
       matrix.add(tile);
-      //                    }
+      // }
     }
     return matrix;
   }
 }
 
+/**
+ * @function addLayer
+ * UNUSED FUNCTION
+ */
 async function addLayer(x, y, w, h) {
   // console.log("Getting Feature Data");
+  const datum = {};
   // const datum = await featureSrcUrl(x, y, w, h);
-  console.log('Creating Feature Layer');
+
+  console.log("Creating Feature Layer");
   // const feature = new FeatureLayer(datum);
-  // scene.add( feature );
+  // scene.add(feature);
+
   const feature2 = new FeatureLayer2(datum);
   scene.add(feature2);
+
   console.log("Segmentation Layer Added");
 }
 
+// SET UP THREE.JS
 THREE.Cache.enabled = true;
 const scene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, -10, 100);
 camera.position.set(0, 0, 150000);
 // camera.position.set(0, 0, 1250);
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
 window.addEventListener('resize', onWindowResize);
+
 const controls = new THREE.TrackballControls(camera, renderer.domElement);
 
-const image = new Rapture(0, 0, 82984, 82984, 0);
+// NEW RAPTURE
+const image = new Rapture(0, 0, magicNumber, magicNumber, 0);
 scene.add(image);
-const feature2 = new FeatureLayer2(0, 0, 82984, 82984, 1);
+
+// NEW FEATURE LAYER
+const feature2 = new FeatureLayer2(0, 0, magicNumber, magicNumber, 1);
 scene.add(feature2);
 
 console.log("Segmentation Layer Added");
 
-// addLayer(0, 0, 82984, 82984);
+// addLayer(0, 0, magicNumber, magicNumber);
+
+/**
+ * ANIMATE
+ */
 function animate() {
+  // <sarcasm>Yeah, this is a good idea. This ordering.</sarcasm>
   controls.update();
   renderer.render(scene, camera);
   requestAnimationFrame(animate);
