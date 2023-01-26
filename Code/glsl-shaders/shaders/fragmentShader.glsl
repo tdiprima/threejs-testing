169b68ed7cba969abda18d353a77e8759fa126d8
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-01-26 15:58:39 -0500

    A plain plane - glsl

--- Code/glsl-shaders/shaders/fragmentShader.glsl
+++ Code/glsl-shaders/shaders/fragmentShader.glsl
@@ -1,10 +1,11 @@
-// https://youtu.be/xZM8UJqN1eY?t=745
+// Fragment shaders: https://youtu.be/xZM8UJqN1eY?t=745
+// Uniforms: https://youtu.be/xZM8UJqN1eY?t=929
 uniform sampler2D tDiffuse;
 uniform float amount;
 varying vec2 vUv;
 
 void main() {
   vec4 color = texture2D(tDiffuse, vUv);
-  // gl_FragColor = texture2D(textureSampler, vUV);
+  // gl_FragColor = contains the color of 1 fragment
   gl_FragColor = color * amount;
 }
--- Code/glsl-shaders/shaders/vertexShader.glsl
+++ Code/glsl-shaders/shaders/vertexShader.glsl
@@ -3,7 +3,8 @@ varying vec2 vUv;
 
 // Takes care of every vertex position in the given mesh
 void main() {
-  vUv = uv;
+  vUv = uv; // clicked-coordinate
   // Calculate the position of the current vertex
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  // So this guy isn't modifying the points; it's just mapping them.
 }
