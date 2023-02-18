898cd4275a04c0429cb495d1c64f3052f9e7d2f6
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-02-18 11:48:18 -0500

    mmm

--- Code/interactive.md
+++ Code/interactive.md
@@ -22,3 +22,21 @@ In this example, annotations are represented as red circles.
 Note that the program includes the [OrbitControls](interactive-image-annotation.html#L45) library for easy camera movement.
 
 The image is loaded using a [TextureLoader](interactive-image-annotation.html#L47), and a [CanvasTexture](interactive-image-annotation.html#L74) is used for the drawing and annotation canvas.
+
+<hr>
+
+The mesh that was created in loader.load is undefined outside the closure.  Can you rewrite the code so that mesh is not undefined when we get to "mesh.add(annotationMesh)"?
+
+To make `mesh` defined outside the `loader.load` closure, we can declare it in the parent scope and assign its value inside the `onLoad` function. Here's an updated version of the code:
+
+```js
+// Code.
+```
+
+In this completed code, we added two new functions `addAnnotationMesh` and `addAnnotation`.
+
+The `addAnnotationMesh` function creates a small red plane at the specified annotation position and adds it to the mesh object.
+
+The `addAnnotation` function is called when the user clicks on the canvas. It calculates the intersection point of the click with the plane and adds an annotation to the `annotations` array. It also calls the `addAnnotationMesh` function to create a red plane at the annotation position.
+
+Finally, we added an event listener for `mousedown` on the window, which calls the `addAnnotation` function. This allows the user to add annotations by clicking anywhere on the canvas.
---
+++ Code/two.js
@@ -0,0 +1,158 @@
+let scene, camera, renderer;
+let mesh, texture, canvasTexture, canvas, context, isDrawing;
+let annotations = [];
+
+init();
+animate();
+
+function init() {
+  scene = new THREE.Scene();
+
+  camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+  camera.position.z = 2;
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+  const loader = new THREE.TextureLoader();
+
+  loader.load(
+    'image.jpg',
+    function (texture) {
+      const geometry = new THREE.PlaneGeometry(1, 1);
+      canvas = document.createElement('canvas');
+      canvas.width = texture.image.width;
+      canvas.height = texture.image.height;
+      context = canvas.getContext('2d');
+      canvasTexture = new THREE.CanvasTexture(canvas);
+      const material = new THREE.MeshBasicMaterial({ map: canvasTexture });
+      mesh = new THREE.Mesh(geometry, material);
+      scene.add(mesh);
+    },
+    undefined,
+    function (error) {
+      console.error('An error happened', error);
+    }
+  );
+
+  isDrawing = false;
+
+  window.addEventListener('mousedown', startDrawing);
+  window.addEventListener('mousemove', draw);
+  window.addEventListener('mouseup', stopDrawing);
+}
+
+function startDrawing(event) {
+  isDrawing = true;
+  currentX = event.offsetX;
+  currentY = event.offsetY;
+}
+
+function draw(event) {
+  if (isDrawing) {
+    let x = event.offsetX;
+    let y = event.offsetY;
+    context.beginPath();
+    context.moveTo(currentX, currentY);
+    context.lineTo(x, y);
+    context.stroke();
+    currentX = x;
+    currentY = y;
+  }
+}
+
+function stopDrawing(event) {
+  isDrawing = false;
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  drawAnnotations();
+}
+
+function drawAnnotations() {
+  if (!mesh) return;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < annotations.length; i++) {
+    let x = annotations[i].x * canvas.width;
+    let y = (1 - annotations[i].y) * canvas.height;
+    context.beginPath();
+    context.arc(x, y, 5, 0, Math.PI * 2);
+    context.fillStyle = '#FF0000';
+    context.fill();
+  }
+  canvasTexture.needsUpdate = true;
+}
+
+window.addEventListener('mousedown', addAnnotation);
+
+function addAnnotation(event) {
+  if (!mesh) return;
+  let rect = event.target.getBoundingClientRect();
+  let x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+  let y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
+  let intersection = getIntersection(x, y);
+  if (intersection) {
+    let annotation = {
+      x: intersection.uv.x,
+      y: intersection.uv.y
+    };
+    annotations.push(annotation);
+  }
+}
+
+// function getIntersection(x, y) {
+//   const raycaster = new THREE.Raycaster();
+//   raycaster.setFromCamera({ x, y }, camera);
+//   const intersects = raycaster.intersectObjects([mesh]);
+//   if (intersects.length > 0) {
+
+function getIntersection(x, y) {
+  const raycaster = new THREE.Raycaster();
+  raycaster.setFromCamera({ x, y }, camera);
+  const intersects = raycaster.intersectObjects([mesh]);
+  if (intersects.length > 0) {
+    return intersects[0];
+  } else {
+    return null;
+  }
+}
+
+function addAnnotationMesh(annotation) {
+  let geometry = new THREE.PlaneGeometry(0.2, 0.2);
+  let material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  let annotationMesh = new THREE.Mesh(geometry, material);
+  annotationMesh.position.set(
+    annotation.x * canvas.width - canvas.width / 2,
+    canvas.height / 2 - annotation.y * canvas.height,
+    0.01
+  );
+  mesh.add(annotationMesh);
+}
+
+function addAnnotation(event) {
+  if (!mesh) return;
+  let rect = event.target.getBoundingClientRect();
+  let x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+  let y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
+  let intersection = getIntersection(x, y);
+  if (intersection) {
+    let annotation = {
+      x: intersection.uv.x,
+      y: intersection.uv.y,
+    };
+    annotations.push(annotation);
+    addAnnotationMesh(annotation);
+  }
+}
+
+
