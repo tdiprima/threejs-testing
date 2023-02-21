dde7cdba3e2eba33cc7e0c8409dd75ea485caf31
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-02-21 16:39:27 -0500

    up

---
+++ Code/AI_Interactive/change-pix-colors.js
@@ -0,0 +1,59 @@
+// Create a new image object
+var image = new Image();
+
+// Set the source of the image
+image.src = 'https://example.com/image.png';
+
+// Wait for the image to load
+image.onload = function() {
+
+  // Create a canvas element
+  var canvas = document.createElement('canvas');
+  canvas.width = image.width;
+  canvas.height = image.height;
+
+  // Get the 2D context of the canvas
+  var context = canvas.getContext('2d');
+
+  // Draw the image on the canvas
+  context.drawImage(image, 0, 0);
+
+  // Get the pixel data of the canvas
+  var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+  var data = imageData.data;
+
+  // Manipulate each pixel
+  for (var i = 0; i < data.length; i += 4) {
+    // Get the red, green, blue, and alpha values of the pixel
+    var red = data[i];
+    var green = data[i + 1];
+    var blue = data[i + 2];
+    var alpha = data[i + 3];
+
+    // Manipulate the red, green, blue, and alpha values as desired
+    // For example, set the red value to 255 to create a red tint
+    data[i] = 255;
+
+    // Alternatively, invert the colors by subtracting the current values from 255
+    data[i] = 255 - red;
+    data[i + 1] = 255 - green;
+    data[i + 2] = 255 - blue;
+  }
+
+  // Set the modified pixel data back on the canvas
+  context.putImageData(imageData, 0, 0);
+
+  // Create a new texture from the modified canvas
+  var texture = new THREE.Texture(canvas);
+  texture.needsUpdate = true;
+
+  // Use the texture in a material
+  var material = new THREE.MeshBasicMaterial({
+    map: texture
+  });
+
+  // Create a mesh and add it to the scene
+  var mesh = new THREE.Mesh(geometry, material);
+  scene.add(mesh);
+};
+
