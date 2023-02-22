3c01aab449302bac98ae6e8c07bcda6b858d1c01
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-02-22 16:53:52 -0500

    fixed (sorta)

--- Code/AI_Interactive/image-annot-2.js
+++ Code/AI_Interactive/image-annot-2.js
@@ -7,9 +7,8 @@ let isDrawing = false;
 function init() {
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(75,  window.innerWidth / window.innerHeight, 0.1, 1000);
-  // camera.position.z = 2;
-  camera.position.z = 10;
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.z = 1;
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -21,30 +20,31 @@ function init() {
 
   loader.load(
     "../portfolio/squirrel_portfolio/squirrel.jpg",
-    function (texture) {
+    function(texture) {
       const geometry = new THREE.PlaneGeometry(1, 1);
-      canvas = document.createElement('canvas');
+
+      canvas = document.createElement("canvas");
       canvas.width = texture.image.width;
       canvas.height = texture.image.height;
-      context = canvas.getContext('2d');
+
+      context = canvas.getContext("2d");
       canvasTexture = new THREE.CanvasTexture(canvas);
-      const material = new THREE.MeshBasicMaterial({ map: canvasTexture });
+
+      // TODO: TESTING
+      // const material = new THREE.MeshBasicMaterial({ map: canvasTexture });
+      const material = new THREE.MeshBasicMaterial({ map: texture });
       mesh = new THREE.Mesh(geometry, material);
-      // TODO: why is parent: null?  Should be scene.
-      console.log("mesh", mesh);
-      console.log("material", mesh.material);
-      console.log("map", mesh.material.map);
       scene.add(mesh);
     },
     undefined,
-    function (error) {
-      console.error('An error happened', error);
+    function(error) {
+      console.error("An error happened", error);
     }
   );
 
-  window.addEventListener('mousedown', startDrawing);
-  window.addEventListener('mousemove', draw);
-  window.addEventListener('mouseup', stopDrawing);
+  window.addEventListener("mousedown", startDrawing);
+  window.addEventListener("mousemove", draw);
+  window.addEventListener("mouseup", stopDrawing);
 }
 
 function startDrawing(event) {
@@ -79,9 +79,9 @@ function animate() {
 
 function drawAnnotations() {
   if (mesh) {
-    console.log("%cGot Mesh", "color: #ccff00;");
+    // console.log("%cGot Mesh", "color: #ccff00;");
   } else {
-    console.log("%cGot Mesh?", "color: deeppink;");
+    // console.log("%cGot Mesh?", "color: deeppink;");
     return;
   }
 
@@ -91,13 +91,13 @@ function drawAnnotations() {
     let y = (1 - annotations[i].y) * canvas.height;
     context.beginPath();
     context.arc(x, y, 5, 0, Math.PI * 2);
-    context.fillStyle = '#FF0000';
+    context.fillStyle = "#FF0000";
     context.fill();
   }
   canvasTexture.needsUpdate = true;
 }
 
-window.addEventListener('mousedown', addAnnotation);
+window.addEventListener("mousedown", addAnnotation);
 
 // TODO
 // document.getElementById("text-input").addEventListener("blur", addAnnotation)
@@ -130,7 +130,10 @@ function addAnnotation(event) {
 
 function getIntersection(x, y) {
   const raycaster = new THREE.Raycaster();
-  raycaster.setFromCamera({ x, y }, camera);
+  raycaster.setFromCamera({
+    x,
+    y
+  }, camera);
   const intersects = raycaster.intersectObjects([mesh]);
   if (intersects.length > 0) {
     return intersects[0];
@@ -153,9 +156,9 @@ function addAnnotationMesh(annotation) {
 
 function addAnnotation(event) {
   if (mesh) {
-    console.log("%cGot Mesh", "color: #ccff00;");
+    // console.log("%cGot Mesh", "color: #ccff00;");
   } else {
-    console.log("%cGot Mesh?", "color: deeppink;");
+    // console.log("%cGot Mesh?", "color: deeppink;");
     return;
   }
   let rect = event.target.getBoundingClientRect();
@@ -165,7 +168,7 @@ function addAnnotation(event) {
   if (intersection) {
     let annotation = {
       x: intersection.uv.x,
-      y: intersection.uv.y,
+      y: intersection.uv.y
     };
     annotations.push(annotation);
     addAnnotationMesh(annotation);
