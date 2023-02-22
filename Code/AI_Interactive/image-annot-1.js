e014aafb0518ad20391e5683325a0ab76e032b28
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-02-22 16:01:14 -0500

    fixed image-annot-1.js (sort of)

--- Code/AI_Interactive/image-annot-1.js
+++ Code/AI_Interactive/image-annot-1.js
@@ -1,12 +1,10 @@
+// TODO: CANVAS.addEventListener does not work. Using window for now.
 let scene, camera, renderer, controls;
 let texture, material, mesh;
 let canvas, context;
 let isDrawing = false;
 let currentX = 0, currentY = 0;
 let annotations = [];
-
-init();
-animate();
 
 function init() {
   scene = new THREE.Scene();
@@ -20,17 +18,11 @@ function init() {
 
   controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-  let loader = new THREE.TextureLoader();
-  loader.load("./portfolio/squirrel_portfolio/squirrel.jpg", function(texture) {
-    texture.minFilter = THREE.LinearFilter;
-    material = new THREE.MeshBasicMaterial({ map: texture });
-    mesh = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
-    scene.add(mesh);
-
-    // Resize plane to fit image aspect ratio
-    let aspect = texture.image.width / texture.image.height;
-    mesh.scale.set(aspect, 1, 1);
-  });
+  let texture = new THREE.TextureLoader().load("../portfolio/squirrel_portfolio/squirrel.jpg");
+  texture.minFilter = THREE.LinearFilter;
+  material = new THREE.MeshBasicMaterial({ map: texture });
+  mesh = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+  scene.add(mesh);
 
   // Create canvas for drawing and annotations
   canvas = document.createElement("canvas");
@@ -41,10 +33,10 @@ function init() {
   context.lineWidth = 5;
 
   // Event listeners for drawing
-  canvas.addEventListener("mousedown", startDrawing);
-  canvas.addEventListener("mousemove", draw);
-  canvas.addEventListener("mouseup", stopDrawing);
-  canvas.addEventListener("mouseout", stopDrawing);
+  window.addEventListener("mousedown", startDrawing);
+  window.addEventListener("mousemove", draw);
+  window.addEventListener("mouseup", stopDrawing);
+  window.addEventListener("mouseout", stopDrawing);
 
   // Add canvas to scene as texture
   let annotationTexture = new THREE.CanvasTexture(canvas);
@@ -55,12 +47,11 @@ function init() {
 
   let annotationMesh = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), annotationMaterial);
   annotationMesh.position.set(0, 0, -0.01);
-  console.log("mesh", mesh);
-  // TODO: The mesh that was created in loader.load is undefined outside the closure.
   mesh.add(annotationMesh);
 
   // Event listener for annotations
-  canvas.addEventListener("click", function(event) {
+  window.addEventListener("click", function(event) {
+    // console.log("click");
     let x = event.offsetX / canvas.width;
     let y = 1 - (event.offsetY / canvas.height);
     annotations.push({
@@ -78,12 +69,14 @@ function animate() {
 }
 
 function startDrawing(event) {
+  // console.log("start");
   isDrawing = true;
   currentX = event.offsetX;
   currentY = event.offsetY;
 }
 
 function draw(event) {
+  // console.log("draw");
   if (isDrawing) {
     let x = event.offsetX;
     let y = event.offsetY;
@@ -97,10 +90,12 @@ function draw(event) {
 }
 
 function stopDrawing(event) {
+  // console.log("stop");
   isDrawing = false;
 }
 
 function drawAnnotations() {
+  // console.log("draw a");
   context.clearRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < annotations.length; i++) {
     let x = annotations[i].x * canvas.width;
@@ -111,3 +106,6 @@ function drawAnnotations() {
     context.fill();
   }
 }
+
+init();
+animate();
