cea1905d8ab1ca8e87dd360632368b2988ce3ea3
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-06-06 10:17:31 -0400

    fixed canvas event handling?

--- Code/AI_Interactive/annotations/image-annot-1.js
+++ Code/AI_Interactive/annotations/image-annot-1.js
@@ -1,10 +1,10 @@
-// TODO: CANVAS.addEventListener does not work. Using window for now.
 let scene, camera, renderer, controls;
 let texture, material, mesh;
 let canvas, context;
 let isDrawing = false;
 let currentX = 0, currentY = 0;
 let annotations = [];
+const imgUrl = "/Code/portfolio/squirrel_portfolio/squirrel.jpg";
 
 function init() {
   scene = new THREE.Scene();
@@ -16,59 +16,95 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  controls = new THREE.OrbitControls(camera, renderer.domElement);
+  // todo: uncomment controls
+  // controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-  // let loader = new THREE.TextureLoader();
-  // loader.load("/Code/portfolio/squirrel_portfolio/squirrel.jpg", function(texture) {
+  const loader = new THREE.TextureLoader();
+  // loader.load(imgUrl, texture => {
   //   texture.minFilter = THREE.LinearFilter;
   //   material = new THREE.MeshBasicMaterial({ map: texture });
   //   mesh = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
-  //   console.log(mesh)
   //   scene.add(mesh);
+  //
   //   // Resize plane to fit image aspect ratio
-  //   let aspect = texture.image.width / texture.image.height;
+  //   const aspect = texture.image.width / texture.image.height;
+  //   console.log("aspect", aspect); // Aspect ratio is 1
   //   mesh.scale.set(aspect, 1, 1);
   // });
 
-  // Doing it like this instead, allowed the image to be drawn to the scene.  Because now "mesh" is defined.
-  // See above - the log didn't print, so it hadn't even run yet, but we tried to use it.
-  let texture = new THREE.TextureLoader().load("/Code/portfolio/squirrel_portfolio/squirrel.jpg");
+  let texture = loader.load(imgUrl);
   texture.minFilter = THREE.LinearFilter;
   material = new THREE.MeshBasicMaterial({ map: texture });
   mesh = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
   scene.add(mesh);
 
   // Create canvas for drawing and annotations
-  canvas = document.createElement("canvas");
-  canvas.width = 1024; // window.innerWidth
-  canvas.height = 1024; // window.innerHeight
-  context = canvas.getContext("2d");
-  context.strokeStyle = "#FF0000";
+  canvas = document.createElement('canvas');
+  canvas.width = window.innerWidth; // 1024
+  canvas.height = window.innerHeight; // 1024
+  context = canvas.getContext('2d');
+  context.strokeStyle = '#FF0000';
   context.lineWidth = 5;
 
+  function startDrawing(event) {
+    isDrawing = true;
+    currentX = event.offsetX;
+    currentY = event.offsetY;
+  }
+
+  function stopDrawing(event) {
+    isDrawing = false;
+  }
+
+  function drawAnnotations() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for (let i = 0; i < annotations.length; i++) {
+      const x = annotations[i].x * canvas.width;
+      const y = (1 - annotations[i].y) * canvas.height;
+      context.beginPath();
+      context.arc(x, y, 5, 0, Math.PI * 2);
+      context.fillStyle = '#FF0000';
+      context.fill();
+    }
+  }
+
+  function draw(event) {
+    if (isDrawing) {
+      const x = event.offsetX;
+      const y = event.offsetY;
+      context.beginPath();
+      context.moveTo(currentX, currentY);
+      context.lineTo(x, y);
+      context.stroke();
+      currentX = x;
+      currentY = y;
+    }
+  }
+
   // Event listeners for drawing
-  // NOTE: Changed it to "window", because "canvas" was not firing.
-  window.addEventListener("mousedown", startDrawing);
-  window.addEventListener("mousemove", draw);
-  window.addEventListener("mouseup", stopDrawing);
-  window.addEventListener("mouseout", stopDrawing);
+  // Changed: canvas.addEventListener
+  renderer.domElement.addEventListener('mousedown', startDrawing);
+  renderer.domElement.addEventListener('mousemove', draw);
+  renderer.domElement.addEventListener('mouseup', stopDrawing);
+  renderer.domElement.addEventListener('mouseout', stopDrawing);
 
   // Add canvas to scene as texture
-  let annotationTexture = new THREE.CanvasTexture(canvas);
-  let annotationMaterial = new THREE.MeshBasicMaterial({
+  const annotationTexture = new THREE.CanvasTexture(canvas);
+  const annotationMaterial = new THREE.MeshBasicMaterial({
     map: annotationTexture,
     transparent: true
   });
 
-  let annotationMesh = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), annotationMaterial);
+  const annotationMesh = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), annotationMaterial);
   annotationMesh.position.set(0, 0, -0.01);
   mesh.add(annotationMesh);
 
   // Event listener for annotations
-  window.addEventListener("click", function(event) {
-    // console.log("click");
-    let x = event.offsetX / canvas.width;
-    let y = 1 - (event.offsetY / canvas.height);
+  renderer.domElement.addEventListener('click', event => {
+    // todo: mousedown instead of click
+    const x = event.offsetX / canvas.width;
+    const y = 1 - event.offsetY / canvas.height;
+    // const y = 1 - (event.offsetY / canvas.height);
     annotations.push({
       x: x,
       y: y
@@ -79,47 +115,9 @@ function init() {
 
 function animate() {
   requestAnimationFrame(animate);
-  controls.update();
+  // todo: uncomment controls
+  // controls.update();
   renderer.render(scene, camera);
-}
-
-function startDrawing(event) {
-  // console.log("start");
-  isDrawing = true;
-  currentX = event.offsetX;
-  currentY = event.offsetY;
-}
-
-function draw(event) {
-  // console.log("draw");
-  if (isDrawing) {
-    let x = event.offsetX;
-    let y = event.offsetY;
-    context.beginPath();
-    context.moveTo(currentX, currentY);
-    context.lineTo(x, y);
-    context.stroke();
-    currentX = x;
-    currentY = y;
-  }
-}
-
-function stopDrawing(event) {
-  // console.log("stop");
-  isDrawing = false;
-}
-
-function drawAnnotations() {
-  // console.log("draw a");
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  for (let i = 0; i < annotations.length; i++) {
-    let x = annotations[i].x * canvas.width;
-    let y = (1 - annotations[i].y) * canvas.height;
-    context.beginPath();
-    context.arc(x, y, 5, 0, Math.PI * 2);
-    context.fillStyle = "#FF0000";
-    context.fill();
-  }
 }
 
 init();
