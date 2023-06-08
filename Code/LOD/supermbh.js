65409a6573212434ae4162cd3873f8d004cdf062
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-06-08 15:25:10 -0400

    up

--- Code/LOD/supermbh.js
+++ Code/LOD/supermbh.js
@@ -23,30 +23,43 @@ scene.add(pointLight);
 // Create a Level of Detail object
 let lod = new THREE.LOD();
 
+// TODO: play with materials
+function getMaterial(type, color, geometry) {
+  if (type === 1) {
+    return new THREE.Mesh(geometry, new THREE.MeshStandardMaterial({
+      color: color,
+      wireframe: true
+    }));
+  }
+
+  if (type === 2) {
+    return new THREE.Mesh(geometry, new THREE.MeshPhongMaterial({
+      color: color,
+      wireframe: true
+    }));
+  }
+
+  return new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+    color: color,
+    wireframe: true
+  }));
+
+}
+
 // Create different levels of detail (meshes or geometries)
-// MeshBasicMaterial if no light
 let highDetailGeometry = new THREE.SphereGeometry(1, 32, 32);
-let highDetailMesh = new THREE.Mesh(highDetailGeometry, new THREE.MeshStandardMaterial({
-  color: 0xff0000,
-  wireframe: true
-}));
+let highDetailMesh = getMaterial(0, 0xff0000, highDetailGeometry);
 
 let mediumDetailGeometry = new THREE.SphereGeometry(1, 16, 16);
-let mediumDetailMesh = new THREE.Mesh(mediumDetailGeometry, new THREE.MeshStandardMaterial({
-  color: 0x00ff00,
-  wireframe: true
-}));
+let mediumDetailMesh = getMaterial(0, 0x00ff00, mediumDetailGeometry);
 
 let lowDetailGeometry = new THREE.SphereGeometry(1, 8, 8);
-let lowDetailMesh = new THREE.Mesh(lowDetailGeometry, new THREE.MeshStandardMaterial({
-  color: 0x0000ff,
-  wireframe: true
-}));
+let lowDetailMesh = getMaterial(0, 0x0000ff, lowDetailGeometry);
 
 // Add each level of detail to the LOD object
-lod.addLevel(highDetailMesh, 0); // Distance = 0 or closer
+lod.addLevel(highDetailMesh, 0);    // Distance = 0 or closer
 lod.addLevel(mediumDetailMesh, 50); // Distance between 0 and 100
-lod.addLevel(lowDetailMesh, 100); // Distance between 100 and 200
+lod.addLevel(lowDetailMesh, 100);   // Distance between 100 and 200
 
 // Add the LOD object to the scene
 scene.add(lod);
