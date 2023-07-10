93018427583c638b6de531156e356f73c6be44dc
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2023-07-10 12:35:12 -0400

    mmm

--- Code/cameras/side-by-2.js
+++ Code/cameras/side-by-2.js
@@ -1,5 +1,5 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/controls/OrbitControls.js";
 
 function main() {
   const checker = "/textures/images/checker.png";
